6dcbb8d2676d37cce2b1c020648bb085f9e97f1f
Fitz Koch <fw.j.keenan.koch@gmail.com>
2025-11-06 11:53:58 -0500

    tweaked data cleaning, added R code

--- fjk_code.R
+++ fjk_code.R
@@ -1,4 +1,3 @@
-
 # install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies=TRUE)
 library(tidyverse)
 library(rstan)
@@ -7,37 +6,47 @@ library(glue)
 getwd()
 
 
+### Test that R is working properly on WSL ###
 # Sample data
 x <- 1:10
 y <- x^2 + rnorm(10, 0, 5)
 
 # Base R plot
 plot(x, y,
-     main = "Example Plot",
-     xlab = "X values",
-     ylab = "Y values",
-     pch = 19,        # solid circles
-     col = "blue")
+  main = "Example Plot",
+  xlab = "X values",
+  ylab = "Y values",
+  pch = 19, # solid circles
+  col = "blue"
+)
 
 # Add a regression line
 abline(lm(y ~ x), col = "red", lwd = 2)
+###
 
 
 stan_files <- c("general_non_possession.stan")
 
 for (file in stan_files) {
-  cat("starting,", stan_file, "\n")
-  x_h_data <- read.csv("data/general_pct_non_possession/x_h_train.csv")
-  x_a_data <- read.csv("data/general_pct_non_possession/x_a_train.csv")
-  y_data <- read.csv("data/general_pct_non_possession/y_train.csv")
+  cat("starting,", file, "\n")
+  x_h_data <- as.matrix(read.csv("data/general_pct_non_possession/x_h_train.csv"))
+  x_a_data <- as.matrix(read.csv("data/general_pct_non_possession/x_a_train.csv"))
+  y_data <- read.csv("data/general_pct_non_possession/y_train.csv")$plus_minus_home
+
+
+  ## clean any missing data ##
+  complete_idx <- complete.cases(x_h_data, x_a_data, y_data)
+  x_h_data <- x_h_data[complete_idx, ]
+  x_a_data <- x_a_data[complete_idx, ]
+  y_data <- y_data[complete_idx]
 
 
   stan_data <- list(
-      N = length(y_data),
-      K = ncol(x_h_data),
+    N = length(y_data),
+    K = ncol(x_h_data),
     x_h = x_h_data,
     x_a = x_a_data,
-      y = y_data
+    y = y_data
   )
 
 
@@ -45,11 +54,11 @@ for (file in stan_files) {
     file = file,
     data = stan_data,
     chains = 4,
-    iter = 2000, 
+    iter = 2000,
     warmup = 1000,
-    verbose=TRUE,
-    refresh=0,
-    seed=1,
+    verbose = TRUE,
+    refresh = 0,
+    seed = 1,
   )
 
   cat("fit complete \n")
