249fae1b7b103c53e3f7114b1d08e89748a7a60c
Prithaj Nath <prithajnath@gmail.com>
2025-11-10 23:13:42 -0500

    rm comments

--- bayes_ic.py
+++ bayes_ic.py
@@ -10,15 +10,10 @@ ll_total = ll_home.values + ll_away.values
 idata.log_likelihood["ll_total"] = (ll_home.dims, ll_total)
 
 
-# 4. Pass the combined array to WAIC and LOO
-print("Calculating WAIC...")
-# Use the log_likelihood argument
 waic_data = az.waic(idata, var_name="ll_total")
 print("--- WAIC ---")
 print(waic_data)
-print("\n")
 
-print("Calculating PSIS-LOO...")
 # Use the log_likelihood argument
 loo_data = az.loo(idata, var_name="ll_total")
 print("--- PSIS-LOO ---")
